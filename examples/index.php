f714f3165a25f0f844e98ec45b3ae80c6775d328
Brandon Sharp <coder0xff@gmail.com>
2015-11-11 02:26:29 -0500

    Page cleanup

--- examples/index.php
+++ examples/index.php
@@ -1,9 +1,5 @@
 <meta charset='utf-8'/><html><head><title>index.html</title><link rel=StyleSheet href='../css/general.css' type='text/css' /></head><body><?php require('../header.php') ?>
-
-
-
-    Output
-
-
-
+<a href="restructuring-version-information.php">Restructuring version information</a>
+<h2>Heavily commented versions:</h2>
+<a href="z_comments-restructuring-version-information.php">Restructuring version information</a>
 <?php require('../footer.php') ?></body></html>
