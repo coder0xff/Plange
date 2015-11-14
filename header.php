6d45449b9292e38f43a5b304c784a5e32e7ccae1
Brandon Sharp <coder0xff@gmail.com>
2015-11-14 11:51:52 -0500

    fix absolute hrefs

--- header.php
+++ header.php
@@ -3,22 +3,22 @@
 <div id="navigation">
 <h2>Contents</h2>
 <ul>
-<li><a href="http://plange.tech/index.php">Home</a></li>
-<li><a href="http://plange.tech/goals/index.php">Goals</a>
+<li><a href="/index.php">Home</a></li>
+<li><a href="/goals/index.php">Goals</a>
 	<ul>
-		<li><a href="http://plange.tech/goals/first-classing.php">First Classing</a></li>
-		<li><a href="http://plange.tech/goals/strong-connected-model-of-computation">SCSM</a></li>
+		<li><a href="/goals/first-classing.php">First Classing</a></li>
+		<li><a href="/goals/strong-connected-model-of-computation.php">SCSM</a></li>
 	</ul>
 </li>	
-<li><a href="http://plange.tech/documentation/">Documentation</a>
+<li><a href="/documentation/">Documentation</a>
 	<ul>
-		<li><a href="http://plange.tech/documentation/syntax/">Syntax</a></li>
-		<li><a href="http://plange.tech/documentation/keywords/">Keywords</a></li>
+		<li><a href="/documentation/syntax/">Syntax</a></li>
+		<li><a href="/documentation/keywords/">Keywords</a></li>
 	</ul>
 </li>
-<li><a href="http://plange.tech/examples/">Examples</a></li>
-<li><a href="http://plange.tech/contribute.php">Contribute</a></li>
-<li><a href="http://plange.tech/about.php">About</a></li>
+<li><a href="/examples/">Examples</a></li>
+<li><a href="/contribute.php">Contribute</a></li>
+<li><a href="/about.php">About</a></li>
 </ul>
 </div>
 <div id="left-margin">
