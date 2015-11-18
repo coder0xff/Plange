805da3baadc64301b5ceb3c4225062b88d8ac91c
Brandon Sharp <coder0xff@gmail.com>
2015-11-18 00:51:02 -0500

    cleanup keywords page, navigation bar updates

--- documentation/keywords.php
+++ documentation/keywords.php
@@ -1,134 +1,78 @@
-<meta charset='utf-8'/><html><head><title>keywords.html</title><link rel=StyleSheet href='../css/general.css' type='text/css' /></head><body><?php require('../header.php') ?>
+<meta charset='utf-8'/>
+<html>
+	<head>
+		<title>Keywords - Plange</title>
+		<link rel=StyleSheet href='../css/general.css' type='text/css' />
+	</head>
+	<body>
+		<?php require('../header.php') ?>
 
-<div id="sites-toc-undefined" jotid="sites-toc-undefined" style="display:block; margin:5px auto 5px 0; text-align:left; width:250px;" class="goog-toc sites-sidebar-nav">
-<h4 class="">Subpage Listing</h4>
-<div class="nav-toc-content" onclick="JOT_SUBPAGE_click(event)" style="text-align: left;">
-<ul jotid="navList">
-<li class="nav-first"><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/dontcare" jotid="wuid:gx:e4802ce53d77a07" class="sites-navigation-link">_ (dont_care)</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/abstract" jotid="wuid:gx:78b1793ba9d38921" class="sites-navigation-link">abstract</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/address_of" jotid="wuid:gx:29cd5a4c2af56fed" class="sites-navigation-link">address_of</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/alloc" jotid="wuid:gx:5dd4dcaa44692f8" class="sites-navigation-link">alloc</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/args" jotid="wuid:gx:72be0d6c4383873d" class="sites-navigation-link">args</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/arm" jotid="wuid:gx:147efef906d5da1c" class="sites-navigation-link">asm</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/at" jotid="wuid:gx:1a8a5c43697ece07" class="sites-navigation-link">at</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/atomic" jotid="wuid:gx:194fc7bf4e52fe7e" class="sites-navigation-link">atomic</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/box" jotid="wuid:gx:7c03321b6dbb8a27" class="sites-navigation-link">box</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/break" jotid="wuid:gx:176aaf2485050168" class="sites-navigation-link">break</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/case" jotid="wuid:gx:30bcd89dc74c7f02" class="sites-navigation-link">case (deprecated)</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/casts" jotid="wuid:gx:4133a850d8776113" class="sites-navigation-link">casts</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/catch" jotid="wuid:gx:2264f67e017525ab" class="sites-navigation-link">catch</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/const" jotid="wuid:gx:8110206c53031bd" class="sites-navigation-link">const (deprecated)</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/context" jotid="wuid:gx:615b69ce59ef3aee" class="sites-navigation-link">context</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/continue" jotid="wuid:gx:98e8a4506934789" class="sites-navigation-link">continue (removed)</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/ctor" jotid="wuid:gx:2461f36dfa1f0354" class="sites-navigation-link">ctor</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/dereference" jotid="wuid:gx:34c46562121e8ec" class="sites-navigation-link">dereference</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/do" jotid="wuid:gx:30b5c128f4b58d0d" class="sites-navigation-link">do</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/does" jotid="wuid:gx:e06396949ab2d23" class="sites-navigation-link">does (unary/binary relation builder)</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/dynamic_cast" jotid="wuid:gx:463869957cbfc285" class="sites-navigation-link">dynamic_cast</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/elif" jotid="wuid:gx:6563b7b73ac1d2e2" class="sites-navigation-link">elif</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/else" jotid="wuid:gx:330c184ddb5dab49" class="sites-navigation-link">else</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/enum" jotid="wuid:gx:63e79446ae19d2ff" class="sites-navigation-link">enum</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/env" jotid="wuid:gx:27ab1645d0fa50e0" class="sites-navigation-link">env</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/finally" jotid="wuid:gx:712b769101224cc4" class="sites-navigation-link">finally</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/for" jotid="wuid:gx:136ce12a50ccc28f" class="sites-navigation-link">for</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/free" jotid="wuid:gx:2ad98d2d00ee88c9" class="sites-navigation-link">free</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/get" jotid="wuid:gx:4c0bc2a6bf5f3773" class="sites-navigation-link">get</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/if" jotid="wuid:gx:4690ea4773e8dcc2" class="sites-navigation-link">if</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/imperative" jotid="wuid:gx:41480fa68679c2b0" class="sites-navigation-link">imperative</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/implementing" jotid="wuid:gx:7e7ab1da0c13b5de" class="sites-navigation-link">implementing</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/in" jotid="wuid:gx:75ca3aa7f3e61e98" class="sites-navigation-link">in</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/inheriting" jotid="wuid:gx:40ea64a145c27557" class="sites-navigation-link">inheriting</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/internal" jotid="wuid:gx:167f0db9caf62cc3" class="sites-navigation-link">internal</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/is" jotid="wuid:gx:619e54a4b87aad47" class="sites-navigation-link">is</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/json" jotid="wuid:gx:58a0d67deb377487" class="sites-navigation-link">json (uneeded)</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/lock" jotid="wuid:gx:6f361292599981a1" class="sites-navigation-link">lock (deprecated)</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/no_throw" jotid="wuid:gx:48948334538b954b" class="sites-navigation-link">no_throw</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/opaque" jotid="wuid:gx:75172532cf8214a8" class="sites-navigation-link">opaque</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/params" jotid="wuid:gx:248c9e825d568deb" class="sites-navigation-link">params</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/private" jotid="wuid:gx:2583bd6aa34eb4b5" class="sites-navigation-link">private</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/protected" jotid="wuid:gx:6952eb9e5f2f9b4b" class="sites-navigation-link">protected</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/protected_internal" jotid="wuid:gx:1457c59c3671c0a3" class="sites-navigation-link">protected_internal</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/public" jotid="wuid:gx:146ea50744087fa2" class="sites-navigation-link">public</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/pure" jotid="wuid:gx:62394389bbaea6bb" class="sites-navigation-link">pure</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/record" jotid="wuid:gx:6f095795886f9f1c" class="sites-navigation-link">record</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/reinterpret_cast" jotid="wuid:gx:13b055f69c694fde" class="sites-navigation-link">reinterpret_cast</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/stable" jotid="wuid:gx:566aa83b45c08f57" class="sites-navigation-link">stable</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/static" jotid="wuid:gx:16704e559eb7cde5" class="sites-navigation-link">static</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/switch" jotid="wuid:gx:42b8f0cfc73a01f5" class="sites-navigation-link">switch (deprecated)</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/this" jotid="wuid:gx:384f4b89217533f9" class="sites-navigation-link">this</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/this_type" jotid="wuid:gx:173ea5abe6a2ecf4" class="sites-navigation-link">this_type</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/throws" jotid="wuid:gx:a4e2572982ab981" class="sites-navigation-link">throws</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/to" jotid="wuid:gx:3eb6a4faef435247" class="sites-navigation-link">to (binary relation builder)</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/try" jotid="wuid:gx:4403bab887ce86a4" class="sites-navigation-link">try</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/type" jotid="wuid:gx:346b2b872c3c62a3" class="sites-navigation-link">type</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/type_of" jotid="wuid:gx:4765b54b71055973" class="sites-navigation-link">type_of</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/unstable" jotid="wuid:gx:423515378a7909f0" class="sites-navigation-link">unstable</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/until" jotid="wuid:gx:709f627bd56d18b3" class="sites-navigation-link">until</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/volatile" jotid="wuid:gx:9963acc5ee63d9d" class="sites-navigation-link">volatile</a>
-</div></li>
-<li class=""><div dir="ltr" style="padding-left: 5px;"><a href="/documentation/keywords/where" jotid="wuid:gx:392c675fe54a5516" class="sites-navigation-link">where</a>
-</div></li>
-</ul>
-</div>
-</div>
-<?php require('../footer.php') ?></body></html>
+		<h2>Subpage Listing</h2>
+		<ul>
+			<li><a href="/documentation/keywords/dont_care.php">_ (dont_care)</a></li>
+			<li><a href="/documentation/keywords/abstract.php">abstract</a></li>
+			<li><a href="/documentation/keywords/address_of.php">address_of</a></li>
+			<li><a href="/documentation/keywords/alloc.php">alloc</a></li>
+			<li><a href="/documentation/keywords/args.php">args</a></li>
+			<li><a href="/documentation/keywords/arm.php">asm</a></li>
+			<li><a href="/documentation/keywords/at.php">at</a></li>
+			<li><a href="/documentation/keywords/atomic.php">atomic</a></li>
+			<li><a href="/documentation/keywords/box.php">box</a></li>
+			<li><a href="/documentation/keywords/break.php">break</a></li>
+			<li><a href="/documentation/keywords/case.php">case (deprecated)</a></li>
+			<li><a href="/documentation/keywords/casts.php">casts</a></li>
+			<li><a href="/documentation/keywords/catch.php">catch</a></li>
+			<li><a href="/documentation/keywords/const.php">const (deprecated)</a></li>
+			<li><a href="/documentation/keywords/context.php">context</a></li>
+			<li><a href="/documentation/keywords/continue.php">continue (removed)</a></li>
+			<li><a href="/documentation/keywords/ctor.php">ctor</a></li>
+			<li><a href="/documentation/keywords/dereference.php">dereference</a></li>
+			<li><a href="/documentation/keywords/do.php">do</a></li>
+			<li><a href="/documentation/keywords/does.php">does (unary/binary relation builder)</a></li>
+			<li><a href="/documentation/keywords/dynamic_cast.php">dynamic_cast</a></li>
+			<li><a href="/documentation/keywords/elif.php">elif</a></li>
+			<li><a href="/documentation/keywords/else.php">else</a></li>
+			<li><a href="/documentation/keywords/enum.php">enum</a></li>
+			<li><a href="/documentation/keywords/env.php">env</a></li>
+			<li><a href="/documentation/keywords/finally.php">finally</a></li>
+			<li><a href="/documentation/keywords/for.php">for</a></li>
+			<li><a href="/documentation/keywords/free.php">free</a></li>
+			<li><a href="/documentation/keywords/get.php">get</a></li>
+			<li><a href="/documentation/keywords/if.php">if</a></li>
+			<li><a href="/documentation/keywords/imperative.php">imperative</a></li>
+			<li><a href="/documentation/keywords/implementing.php">implementing</a></li>
+			<li><a href="/documentation/keywords/in.php">in</a></li>
+			<li><a href="/documentation/keywords/inheriting.php">inheriting</a></li>
+			<li><a href="/documentation/keywords/internal.php">internal</a></li>
+			<li><a href="/documentation/keywords/is.php">is</a></li>
+			<li><a href="/documentation/keywords/json.php">json (uneeded)</a></li>
+			<li><a href="/documentation/keywords/lock.php">lock (deprecated)</a></li>
+			<li><a href="/documentation/keywords/no_throw.php">no_throw</a></li>
+			<li><a href="/documentation/keywords/opaque.php">opaque</a></li>
+			<li><a href="/documentation/keywords/params.php">params</a></li>
+			<li><a href="/documentation/keywords/private.php">private</a></li>
+			<li><a href="/documentation/keywords/protected.php">protected</a></li>
+			<li><a href="/documentation/keywords/protected_internal.php">protected_internal</a></li>
+			<li><a href="/documentation/keywords/public.php">public</a></li>
+			<li><a href="/documentation/keywords/pure.php">pure</a></li>
+			<li><a href="/documentation/keywords/record.php">record</a></li>
+			<li><a href="/documentation/keywords/reinterpret_cast.php">reinterpret_cast</a></li>
+			<li><a href="/documentation/keywords/stable.php">stable</a></li>
+			<li><a href="/documentation/keywords/static.php">static</a></li>
+			<li><a href="/documentation/keywords/switch.php">switch (deprecated)</a></li>
+			<li><a href="/documentation/keywords/this.php">this</a></li>
+			<li><a href="/documentation/keywords/this_type.php">this_type</a></li>
+			<li><a href="/documentation/keywords/throws.php">throws</a></li>
+			<li><a href="/documentation/keywords/to.php">to (binary relation builder)</a></li>
+			<li><a href="/documentation/keywords/try.php">try</a></li>
+			<li><a href="/documentation/keywords/type.php">type</a></li>
+			<li><a href="/documentation/keywords/type_of.php">type_of</a></li>
+			<li><a href="/documentation/keywords/unstable.php">unstable</a></li>
+			<li><a href="/documentation/keywords/until.php">until</a></li>
+			<li><a href="/documentation/keywords/volatile.php">volatile</a></li>
+			<li><a href="/documentation/keywords/where.php">where</a></li>
+		</ul>
+
+		<?php require('../footer.php') ?>
+	</body>
+</html>
--- header.php
+++ header.php
@@ -4,19 +4,21 @@
 <h2>Contents</h2>
 <ul>
 <li><a href="/index.php">Home</a></li>
-<li><a href="/goals/index.php">Goals</a>
-	<ul>
-		<li><a href="/goals/first-classing.php">First Classing</a></li>
-		<li><a href="/goals/strong-connected-model-of-computation.php">SCSM</a></li>
-	</ul>
-</li>	
+<li><a href="/goals/index.php">Goals</a></li>	
+<li><a href="/examples/">Examples</a></li>
 <li><a href="/documentation/">Documentation</a>
 	<ul>
-		<li><a href="/documentation/syntax/">Syntax</a></li>
-		<li><a href="/documentation/keywords/">Keywords</a></li>
+		<li><a href="/documentation/keywords.php">Keywords</a></li>
+		<li><a href="/documentation/standard-library.php">Standard Library</a></li>
+		<li><a href="/documentation/syntax.php">Syntax</a></li>
+		<li><a href="/documentation/glossary.php">Glossary</a></li>
+		<li><a href="/documentation/operators.php">Opeartors</a></li>
+		<li><a href="/documentation/properties.php">Properties</a></li>
+		<li><a href="/documentation/quote-comment-embedding.php">Embedding</a></li>
+		<li><a href="/documentation/reflection.php">Reflection</a></li>
+		<li><a href="/documentation/using-types.php">Type System</a></li>
 	</ul>
 </li>
-<li><a href="/examples/">Examples</a></li>
 <li><a href="/contribute.php">Contribute</a></li>
 <li><a href="/about.php">About</a></li>
 </ul>
