6b2aa846146d906eb04449702d6e37768b0fd8f9
Brandon Sharp <coder0xff@gmail.com>
2015-11-21 12:12:28 -0500

    Work on Const

--- documentation/standard-library/const.php
+++ documentation/standard-library/const.php
@@ -8,7 +8,10 @@
 		<?php require('../../header.php') ?>
 
 
+		<p>returns a new type wrapping the specified type so that no fields/properties may be written/set and only functions that do not alter state may be called, which includes all pure functions</p>
 
+		<h2>Notes</h2>
+		<p>The returned type also provides an implicit cast back to the non-wrapped (non const) type, iff the wrapped type can be value copied. Due to the limitations of static analysis, violations of Const correctness may not be detected until runtime. Warnings are generated during compile time if static analysis was inconclusive.</p>
 
 
 		<?php require('../../footer.php') ?>
