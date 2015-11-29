92a96d81f2103312bb5bddcba6dc8260516e64bf
Brandon Sharp <coder0xff@gmail.com>
2015-11-29 05:27:31 -0500

    more glossary cleanup

--- documentation/glossary.php
+++ documentation/glossary.php
@@ -11,39 +11,39 @@
 		<h4>algebraic type</h4>
 		<div>an <b>algebraic value</b> that is a <b>type</b></div>
 		<h4>algebraic value</h4>
-		<div>a <b>value</b>&nbsp;defined as the solution(s) of a system of <b>constraints</b></div>
+		<div>a <b>value</b> defined as the solution(s) of a system of <b>constraint</b>s</div>
 		<h4>algorithm</h4>
-		<h4><span>a self-contained set of <b>operations</b>&nbsp;to be performed</span></h4>
+		<div>a self-contained set of <b>operation</b>s to be performed</h4>
 		<h4>collection</h4>
-		<div>a <b>value</b>&nbsp;that stores zero or more other <b>values</b></div>
+		<div>a <b>value</b> that stores zero or more other <b>values</b></div>
 		<h4>compiler</h4>
-		<div>a program that converts&nbsp;<b>source code</b>&nbsp;into a <b>machine code</b>&nbsp;or <b>intermediate representation</b>&nbsp;for <b>execution</b> on a target <b>platform</b>.</div>
+		<div>a program that converts source code into a machine code or intermediate representation for execution on a target <b>platform</b>.</div>
 		<h4>constraint</h4>
-		<div>a <b>value</b> that indicates a logical relationship among two or more other&nbsp;<b>value</b>s, including arithmetic, predicate logic, and other select (though extensible) analysis and calculi</div>
+		<div>a <b>value</b> that indicates a logical relationship among two or more other <b>value</b>s, including arithmetic, predicate logic, and an extensible collection of other analysis and calculi</div>
 		<h4>function</h4>
-		<div>a <b>value</b>&nbsp;that when <b>invoked</b>&nbsp;applies an algorithm (the implementation) to zero or more given values and optionally&nbsp;<b>returns</b>&nbsp;a <b>value</b>&nbsp;as a result</div>
+		<div>a <b>value</b> that when invoked applies an algorithm (the implementation) to zero or more given values and optionally <b>returns</b> a <b>value</b> as a result</div>
 		<h4>function type</h4>
-		<div>a <b>type</b>&nbsp;that describes <b>functions</b>&nbsp;(not to be confused with <b>type function</b>)</div>
+		<div>a <b>type</b> that describes <b>functions</b> (not to be confused with <b>type function</b>)</div>
 		<h4>functional equivalent</h4>
 		<div>a <b>function </b>that can be substituted for another, and produce the same results given the same input, even though the implementation may be different</div>
 		<h4>iterator</h4>
-		<div>a <b>value</b>&nbsp;used to enumerate the elements of a <b>collection</b></div>
+		<div>a <b>value</b> used to enumerate the elements of a <b>collection</b></div>
 		<h4>native program</h4>
-		<div> a <b>program</b>&nbsp;encoded in the manner recognized by the hardware of the computer</div>
+		<div> a <b>program</b> encoded in the manner recognized by the hardware of the computer</div>
 		<h4>operation</h4>
 		<div>a primitive action that reads, writes, or transforms (or some combination thereof) data utilizing the available hardware and software infrastructure</div>
 		<h4>parser</h4>
 		<div>a program that identifies the syntactic structure of a piece of text, using provided grammar rules</div>
 		<h4>platform</h4>
-		<div>the hardware, firmware, <b>software</b>, and data that make up the basic operational components of a computer, and more specifically the formal properties of its <b>implementation</b> and <b>behavior</b> (e.g. <b>memory model</b>, <b>instruction set</b>, <b>calling convention</b>, or <b>application binary interface</b>)</div>
+		<div>the hardware, firmware, <b>software</b>, and data that make up the basic operational components of a computer, and more specifically the formal properties of its implementation and behavior (e.g. memory model, instruction set, calling convention, or application binary interface)</div>
 		<h4>program</h4>
-		<div>formally, a representation of one or more <b>algorithms</b> that may be linearized (made sequential) and executed by a Turing machine, but colloquially any collection of <b>software</b> and&nbsp;data</div>
+		<div>formally, a representation of one or more <b>algorithm</b>s that may be linearized (made sequential) and executed by a Turing machine, but colloquially any collection of <b>software</b> and data</div>
 		<h4>programming language</h4>
 		<div>a set of grammar rules and logical semantics for constructing solutions to computational problems</div>
 		<h4>return (intransitive)</h4>
-		<div>to exit the current <b>function</b>, returning control to the <b>caller</b>,&nbsp;possibly supplying a <b>value</b>&nbsp;as a result</div>
+		<div>to exit the current <b>function</b>, returning control to the caller, possibly supplying a <b>value</b> as a result</div>
 		<h4>return (transitive)</h4>
-		<div>to exit the current <b>function</b>, supplying the object (of the sentence) as a result, e.g. "return 5"</span></div>
+		<div>to exit the current <b>function</b>, supplying the object (of the sentence) as a result, e.g. "return 5"</div>
 		<h4>requirement</h4>
 		<div>an assertion that, from some given starting state and some given operation, some given ending state is derived</div>
 		<h4>runtime</h4>
@@ -51,13 +51,13 @@
 		<h4>software</h4>
 		<div>any collection of well-defined algorithms, typically interrelated and with the intent of fulfilling some <b>specification</b></div>
 		<h4>specification</h4>
-		<div>an (ideally) exhaustive description of the structure and behavior of hardware&nbsp;and/or <b>software</b>, typically in the form of written natural language and accompanying images, to be referenced in the construction of such wares</div>
+		<div>an (ideally) exhaustive description of the structure and behavior of hardware and/or <b>software</b>, typically in the form of written natural language and accompanying images, to be referenced in the construction of such wares</div>
 		<h4>standard library</h4>
-		<div>the collection of basic&nbsp;<b>software</b>&nbsp;building-blocks included with a <b>programming language</b></div>
+		<div>the collection of basic <b>software</b> building-blocks included with a <b>programming language</b></div>
 		<h4>type</h4>
-		<div>a <b>value</b>&nbsp;describing the structure and behavior of another&nbsp;<b>value</b></div>
+		<div>a <b>value</b> describing the structure and behavior of another <b>value</b></div>
 		<h4>type function</h4>
-		<div>a <b>function</b>&nbsp;that <b>returns</b> a <b>type</b>&nbsp;(not to be confused with function type)</div>
+		<div>a <b>function</b> that <b>returns</b> a <b>type</b> (not to be confused with function type)</div>
 		<h4>value</h4>
 		<div>formally, any entity such that equality between two such entities is well defined; informally, a thing that is unlike all other things, and only like itself<br>
 		</div>
