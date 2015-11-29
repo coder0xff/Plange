73794f16210a597784cd9eaad7059f86b2cf9ff6
Brandon Sharp <coder0xff@gmail.com>
2015-11-29 05:11:56 -0500

    random fixes

--- contribute.php
+++ contribute.php
@@ -11,7 +11,7 @@
 		<p>Plange is an ambitious computer science and engineering project, still in the earliest stages.
 		As an open source and public effort, participation is enthusiastically encouraged.
 		Contributions from any and all skill levels are necessary and <i>enthusiatically</i> encourage.
-		There should be enough of the site already filled in to help you <a href="home.php">get a feel</a>
+		There should be enough of the site already filled in to help you <a href="/">get a feel</a>
 		for the project.</p>
 		</p>Please send any suggestions, critique, or questions to coder0xff on the gmail domain. @coder0xff on twitter.</p>
 		<h2>Status</h2>
