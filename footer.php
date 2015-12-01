5210d54455c5ce869448b235f8d918ba4fc0512a
Brandon Sharp <coder0xff@gmail.com>
2015-11-30 20:46:05 -0500

    Add hit counter to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,24 @@
 	</div>
 </div>
 <hr>
-<center>copyright Brent Lewis 2015</center>
+<center>
+	copyright © Brent Lewis 2015 &nbsp;&nbsp;&nbsp;&nbsp; hits: 
+<?php
+//Easy Counter code begins
+
+$ec_username = 'coder0xff'; // <--- your username
+
+if (!$ec_fsock = fsockopen('www.easycounter.com', 80, $errno, $errstr, 2)) {
+  echo '<img src="http://www.easycounter.com/images/error.png">';
+} else {
+  fputs($ec_fsock, "GET /php.counter.php?username=".urlencode($ec_username)." HTTP/1.0\r\n".
+  "Host:www.easycounter.com\r\n\r\n");
+  $ec_buffer = '';
+  while (!feof($ec_fsock)) $ec_buffer .= fgets($ec_fsock, 1024);
+  echo substr($ec_buffer, strpos($ec_buffer, "\n\r\n")+3);
+  fclose($ec_fsock);
+}
+
+//Easy Counter code ends
+?>
+</center>
