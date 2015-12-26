f00504f6abf8ef4ec488c83ff2fc516af34e091e
Brandon Sharp <coder0xff@gmail.com>
2015-12-25 23:34:11 -0500

    add bool_constant.php

---
+++ documentation/syntax/bool_constant.php
@@ -0,0 +1,20 @@
+<meta charset='utf-8'/>
+<html>
+	<head>
+		<title>bool_constant - Plange</title>
+		<link rel=StyleSheet href='../../css/general.css' type='text/css' />
+	</head>
+	<body>
+		<?php require('../../header.php') ?>
+
+
+		<div class="code">
+			<p>Syntax</p>
+			<pre>"true" | "false"</pre>
+		</div>
+		<p>These are included as syntax elements because they are literals, not constants.</p>
+
+
+		<?php require('../../footer.php') ?>
+	</body>
+</html>
