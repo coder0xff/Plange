032849210a0f862f2a31a843ae2e905a3ec9dedf
blewis <coder0xff@gmail.com>
2017-06-15 20:45:03 +0000

    website updates

---
+++ documentation/standard-library/Allocators.php
@@ -0,0 +1,25 @@
+<meta charset='utf-8'/>
+<meta name="viewport" content="width=device-width, initial-scale=0.6">
+<meta charset='utf-8'/>
+<meta name="viewport" content="width=device-width, initial-scale=0.6">
+<html>
+	<head>
+		<title>Plange.Runtime - Plange</title>
+		<link rel=StyleSheet href='../../css/general.css' type='text/css' />
+	</head>
+	<body>
+		<?php require('../../header.php') ?>
+
+
+		<h2>Types</h2>
+		<code>
+			<table style="width:2000px;">
+				<tr><td>BuddySystem</td></tr>
+				<tr><td>Heap</td></tr>
+			</table>
+		</code>
+		
+
+		<?php require('../../footer.php') ?>
+	</body>
+</html>
---
+++ documentation/standard-library/Structures.php
@@ -0,0 +1,51 @@
+<meta charset='utf-8'/>
+<meta name="viewport" content="width=device-width, initial-scale=0.6">
+<meta charset='utf-8'/>
+<meta name="viewport" content="width=device-width, initial-scale=0.6">
+<html>
+	<head>
+		<title>Plange.Structures - Plange</title>
+		<link rel=StyleSheet href='../../css/general.css' type='text/css' />
+	</head>
+	<body>
+		<?php require('../../header.php') ?>
+
+
+		<h2>Type Functions</h2>
+		<code>
+			<table style="width:2000px;">
+				<tr><td>AATree</td></tr>
+				<tr><td>BPlusTree</td></tr>
+				<tr><td>BStarTree</td></tr>
+				<tr><td>BTree</td></tr>
+				<tr><td>Beap</td></tr>
+				<tr><td>BloomFilter</td></tr>
+				<tr><td>CircularBuffer</td></tr>
+				<tr><td>ConcTreeList</td></tr>
+				<tr><td>DancingTree</td></tr>
+				<tr><td>DoublyLinkedList</td></tr>
+				<tr><td>FibonacciHeap</td></tr>
+				<tr><td>FusionTree</td></tr>
+				<tr><td>GapBuffer</td></tr>
+				<tr><td>IliffeVector</td></tr>
+				<tr><td>KAryNode</td></tr>
+				<tr><td>Queap</td></tr>
+				<tr><td>RadixTree</td></tr>
+				<tr><td>RedBlackTree</td></tr>
+				<tr><td>Rope</td></tr>
+				<tr><td>ScapegoatTree</td></tr>
+				<tr><td>SkipList</td></tr>
+				<tr><td>&lt;Type * Array&lt;Int&gt; → &gt; SparseArray</td></tr>
+				<tr><td>SuffixTree</td></tr>
+				<tr><td>Treap</td></tr>
+				<tr><td>Trie</td></tr>
+				<tr><td>WeakHeap</td></tr>
+				<tr><td>XorLinkedList</td></tr>
+				<tr><td>Zipper</td></tr>
+			</table>
+		</code>
+		
+
+		<?php require('../../footer.php') ?>
+	</body>
+</html>
