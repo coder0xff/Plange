437361aae4e572cfb9c6fdd18b7bf2c5cca6e107
coder0xff <coder0xff@gmail.com>
2017-12-23 14:54:30 -0500

    Add mpl::apply_v_constructor.

--- source/mpl/mpl_apply_v.hpp
+++ source/mpl/mpl_apply_v.hpp
@@ -4,6 +4,7 @@
 #include <utility>
 
 #include "mpl_utils.hpp"
+#include "mpl_indices.hpp"
 
 namespace mpl {
 
@@ -21,17 +22,28 @@ namespace mpl {
 		 	return functor(std::get<Indices>(std::forward<TTuple>(t))...);
 		}
 
+		template <typename T, typename TTuple, size_t... Indices>
+		constexpr auto impl_ctor(TTuple && t, std::index_sequence<Indices...>) {
+			return T(std::get<Indices>(std::forward<TTuple>(t))...);
+		}
 	}
 
 	template<typename TFunctor, typename TTuple>
-	constexpr auto apply_values(TFunctor && functor, TTuple && elements) {
+	constexpr auto apply_v(TFunctor && functor, TTuple && elements) {
 		return impl(
 			std::forward<TFunctor>(functor),
 			std::forward<TTuple>(elements),
-			detail::sequence(std::forward<TTuple>(elements))
+			indices_of<TTuple>()
 		);
 	}
 
+	template<typename T, typename TTuple>
+	constexpr auto apply_v_constructor(TTuple && elements) {		
+		return detail::apply_values::impl_ctor<T>(
+			std::forward<TTuple>(elements),
+			indices_of<TTuple>()
+		);
+	}
 }
 
 #define INCLUDED_MPL_APPLY_VALUES_HPP
