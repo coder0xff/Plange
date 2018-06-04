e349a45883b70492f5a3a6ef6056f2a0f79921e6
coder0xff <coder0xff@gmail.com>
2018-06-04 09:27:01 -0400

    Update contribute.php.

--- contribute.php
+++ contribute.php
@@ -9,21 +9,15 @@
 		<?php require('header.php') ?>
 
 		<h2>Welcome!</h2>
-		If you're looking to contribute to open source, please allow me to offer up a project for your consideration. It's called Plange. It began in August 2013 as an effort to create a revolutionary development experience, including a programming language with some exciting new ideas. There is still plenty of work to be done, suitable for any and all skill levels. Please peruse this website, which is the dedicated source of information and webservices for the project. Thanks for your time!
+		If you're looking to contribute to open source, allow me to offer a project for your consideration. It's called Plange. It began in August 2013 as an effort to create a revolutionary development experience, including a programming language with some exciting new ideas. There is still plenty of work to be done, suitable for any and all skill levels. Please peruse this website, which is the dedicated source of information and webservices for the project. Thanks for your time!
 
-		<h2>ACTIVELY LOOKING</h2>
-		<p>We're currently looking for a build engineer to setup Travis CI, Coverity, and/or Codacity within the GitHub repo. Please contact me directly if you're interested. See contact info below.</p>
-		
-		<h2>Short & Simple</h2>
-		<p>Study the examples around the site. There are a lot of them hidden away in the various pages. If you see anything that looks fishy, contradictory, or inconsistent please send me a message.</p>
+		<h2>Active Work (November 21, 2017)</h2>
+		<ul>
+			<li>Continuous Integration with Jenkins, Codacy.</li>
+			<li>C++ bootstrap code generation with LLVM</li>
+		<ul>
 
-		<h2>Deeper contributions</h2>
-		<p>Plange is an ambitious computer science and engineering project, still in early stages. As an open source and public effort, participation is enthusiastically encouraged. View the documentation pages, check out the code from <a href="https://github.com/coder0xff/Plange">github</a>, and start submitting pull requests.</p>
-
-		</p>Please send any comments, critique, or questions to coder0xff on the gmail domain or @coder0xff on twitter.</p>
-
-		<h2>Status (November 21, 2017)</h2>
-		<p>The bootstrapping compiler, written in C++17, is in development. Programs can be loaded into memory from source code. The current effort is LLVM code generation.</p>
+		<p>View the <a href="plange.tech/documentation">documentation pages</a>, check out the code from <a href="https://github.com/coder0xff/Plange">github</a>, and start submitting pull requests. Contact me at coder0xff on the gmail domain or @coder0xff on twitter.</p>
 
 		<?php require('footer.php') ?>
 	</body>
