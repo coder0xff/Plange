8b3ebe13f82bafdde69e700fa56680eecb73fe3b
coder0xff <coder0xff@gmail.com>
2018-06-04 09:27:19 -0400

    udpate header.php.

--- header.php
+++ header.php
@@ -48,36 +48,3 @@
 </div>
 <div id="left-margin">
 	<div id="contents">
-<?php
-    function TableOfContents($depth)
-    /*AutoTOC function written by Alex Freeman
-    * Released under CC-by-sa 3.0 license
-    * http://www.10stripe.com/  */
-    {
-    $filename = debug_backtrace()[0]["file"];
-    //read in the file
-    $file = fopen($filename,"r");
-    $html_string = fread($file, filesize($filename));
-    fclose($file);
- 
-    //get the headings down to the specified depth
-    $pattern = '/<h[2-'.$depth.']*[^>]*>.*?<\/h[2-'.$depth.']>/';
-    $whocares = preg_match_all($pattern,$html_string,$winners);
- 
-    //reformat the results to be more usable
-    $heads = implode("\n",$winners[0]);
-    $heads = str_replace('<a name="','<a href="#',$heads);
-    $heads = str_replace('</a>','',$heads);
-    $heads = preg_replace('/<h([1-'.$depth.'])>/','<li class="toc$1">',$heads);
-    $heads = preg_replace('/<\/h[1-'.$depth.']>/','</a></li>',$heads);
- 
-    //plug the results into appropriate HTML tags
-    $contents = '<div id="toc"> 
-    <p>Contents</p>
-    <ul>
-    '.$heads.'
-    </ul>
-    </div>';
-    echo $contents;
-    }
- ?>
