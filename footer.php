a3629085808a8073e9f796be7116737914931f22
coder0xff <coder0xff@gmail.com>
2018-06-04 09:27:10 -0400

    Update footer.php.

--- footer.php
+++ footer.php
@@ -2,6 +2,6 @@
 </div>
 <hr>
 <center>
-	This website does not use cookies or javascript, excluding the Google search bar.<br>
+	This website does not use cookies or javascript, excluding the Google search bar and Google analytics.<br>
 	copyright © Brent Lewis 2013-2018
 </center>
