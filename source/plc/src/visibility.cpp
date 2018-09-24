a2b91058178fb9ca725d8e4b27af2708492cab1e
coder0xff <coder0xff@gmail.com>
2018-09-24 17:49:51 -0400

    Add visibility enum to plc.

---
+++ source/plc/src/visibility.cpp
@@ -0,0 +1,34 @@
+#include "visibility.hpp"
+
+#include "VISIBILITY_MODIFIER.hpp"
+
+namespace plc {
+
+visibility visibility_from_ast(VISIBILITY_MODIFIER const & value) {
+	struct visitor {
+		visibility operator()(parlex::detail::document::text<literal_public_t>) const {
+			return visibility::PUBLIC;
+		}
+	
+		visibility operator()(parlex::detail::document::text<literal_protected_t>) const {
+			return visibility::PROTECTED;
+		}
+	
+		visibility operator()(parlex::detail::document::text<literal_protected0x20internal_t>) const {
+			return visibility::PROTECTED_INTERNAL;
+		}
+	
+		visibility operator()(parlex::detail::document::text<literal_internal_t>) const {
+			return visibility::INTERNAL;
+		}
+	
+		visibility operator()(parlex::detail::document::text<literal_private_t>) const {
+			return visibility::PRIVATE;
+		}
+	};
+
+	VISIBILITY_MODIFIER_base const & base = value;
+	return std::visit(visitor(), base);
+}
+
+}
