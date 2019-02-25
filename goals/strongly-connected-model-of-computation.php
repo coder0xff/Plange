1631ea3e4f7da7c836562cb900f47926a8ff3a54
Brent Lewis <coder0xff@gmail.com>
2019-02-25 15:45:13 -0500

    Update strongly-connected-model-of-computation.php

--- goals/strongly-connected-model-of-computation.php
+++ goals/strongly-connected-model-of-computation.php
@@ -14,6 +14,15 @@
 		<p>Bootstrapping a compiler is a monumental achievement. Like all programs, compilers start as source code. Source code is compiled, by a compiler, into a computer program. In the case of bootstrapping, the developer compiles the compiler — using itself. This creates a cycle in the computational dependency digraph described above. More importantly, it makes the digraph <a href="http://mathworld.wolfram.com/StronglyConnectedDigraph.html">strongly connected.</a> While difficult to achieve, it demonstrates that the differences between runtime computations and compile time computations can be reduced to zero.</p>
 		<p>Following this, the distinctions between compile time and runtime become less relevant. The only distinction to be made from here is that some computations can be performed now (which may mean statically, at “compile time”), and some cannot. If the runtime environment maintains the strong digraph, then programs can perform any task that the original toolchain could, including self-modification. This is why it is desirable to bootstrap the entire toolchain. At the very least, the toolchain must be capable of taking the source code of the entire toolchain and building itself.</p>
 		<p>As you move down the abstraction layers, the components of the bootstrapped compiler are revealed, but the graph remains strongly connected. Several features arise from this concept, such as continuous profile guided optimization, dependency injection, and much more.
+		<h2>Applications</h2>
+		<ul>
+			<li>Obfuscation, anti-crack</li>
+			<li>DSL compilation</li>
+			<li>Dependency validation</li>
+			<li>Production troubleshooting</li>
+			<li>Game engines</li>
+			<li>Plugin support</li>
+		</ul>
 		<h2>Advantages</h2>
 		<ul>
 			<li>Improves upon Eval using homoiconicity and metaobject concepts (everything is data)</li>
