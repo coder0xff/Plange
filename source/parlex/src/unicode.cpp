df237fc81ce57a7c185ad795755ab68d3d5fce96
coder0xff <coder0xff@gmail.com>
2020-02-13 12:45:46 -0500

    Remove parlex

--- source/parlex/src/unicode.cpp
+++
@@ -1,9 +0,0 @@
-#include "parlex/detail/unicode.hpp"
-
-#define U32(name, condition) \
-bool parlex::detail::name##_t::test(std::u32string const& document, uint32_t documentPosition) const { \
-	auto const & i = document[documentPosition]; \
-	return condition; \
-}
-
-#include "parlex/detail/unicode.inc"
